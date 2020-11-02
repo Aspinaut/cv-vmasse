edab9dbbcc73623fa7f18e389e050c7a6c2923be
Vincent Masse <vincentmasse3@gmail.com>
2020-11-02 18:08:57 +0100

    responsive ok

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
   </head>
   <body>
-    <div class="container-fluid background-image">
+    <div class="d-flex container-fluid background-image">
       <img class="img-fluid" src="IMG_20201024_090805.png" alt="Avatar">
 
     </div>
@@ -17,46 +17,35 @@
         <h3>Développeur web</h3>
         <a href="CV_VincentMasse.pdf" download>Télécharger PDF</button>
         <div class="dropdown">
-          <a type="button" class="btn btn-warning" href="#links">Liens</a>
-          <button type="button" class="btn btn-info dropdown-toggle" href="#contact" data-toggle="dropdown" id="dropdownMenu">Contact</button>
+          <a type="button" class="btn btn-warning" href="#contact">Contact</a>
+          <button type="button" class="btn btn-info dropdown-toggle" href="#links" data-toggle="dropdown" id="dropdownMenu">Liens</button>
           <div class="dropdown-menu" aria-labelledby="dropdownMenu">
-            <p class="dropdown-item">MASSE Vincent</p>
-            <p class="dropdown-item">102, rue du Gras Boeuf</p>
-            <p class="dropdown-item">n°81, résidence les 5 pommes</p>
-            <p class="dropdown-item">59163 Condé sur l'Escaut</p>
-            <p class="dropdown-item">+33 6 28 53 65 98</p>
-            <p class="dropdown-item">vincentmasse@protonmail.com</p>
+            <p class="dropdown-item">
+              <i class="fab fa-github"></i>
+               <a href="https://github.com/Aspinaut">https://github.com/Aspinaut</a></p>
+            <p class="dropdown-item">
+              <i class="fab fa-linkedin"></i>
+              <a href="https://www.linkedin.com/in/vincent-masse-701a931a3/">https://www.linkedin.com/in/vincent-masse-701a931a3/</a></p>
+            <p class="dropdown-item">
+              <span class="underline">Projet Final de formation :</span>
+              <a href="https://thp-final-project.herokuapp.com">https://thp-final-project.herokuapp.com</a></p>
           </div>
         </div>
       </div>
-      <!--
-      <div class="d-flex text-center cv-header">
-        <div class="col-4">
-          <button type="button" class="btn btn-primary  mr-5" href="#">Liens</button>
-        </div>
-        <div class="col-4 titles">
-          <h1>Vincent MASSE</h1>
-          <h3>Développeur web</h3>
-        </div>
-        <div class="col-4">
-          <button type="button" class="btn btn-primary ml-5" href="#">Contact</button>
-        </div>
-      </div>
--->
       <div class="cv-body">
         <nav class="mb-3 mt-5 navbar">
           <div class="nav nav-tabs" id="nav-tab" role="tablist">
-            <a class="nav-item nav-link" id="nav-skills-tab" data-toggle="tab" href="#nav-skills" role="tab" aria-controls="nav-skills" aria-selected="true" onclick="manageNavbarContent('nav-skills')">Compétences</a>
+            <a class="nav-item nav-link" id="nav-skills-tab" data-toggle="tab" href="#nav-skills" role="tab" aria-controls="nav-skills" aria-selected="true" >Compétences</a>
             <a class="nav-item nav-link" id="nav-trainings-tab" data-toggle="tab" href="#nav-trainings" role="tab" aria-controls="nav-trainings" aria-selected="false"
-            onclick="manageNavbarContent('nav-trainings')">Formations</a>
-            <a class="nav-item nav-link" id="nav-xp-tab" data-toggle="tab" href="#nav-xp" role="tab" aria-controls="nav-xp" aria-selected="false" onclick="manageNavbarContent('nav-xp')">Expériences</a>
-            <a class="nav-item nav-link" id="nav-languages-tab" data-toggle="tab" href="#nav-languages" role="tab" aria-controls="nav-languages" aria-selected="false" onclick="manageNavbarContent('nav-languages')">Langues</a>
-            <a class="nav-item nav-link" id="nav-passions-tab" data-toggle="tab" href="#nav-passions" role="tab" aria-controls="nav-passions" aria-selected="false" onclick="manageNavbarContent('nav-passions')">Passions</a>
+            >Formations</a>
+            <a class="nav-item nav-link" id="nav-xp-tab" data-toggle="tab" href="#nav-xp" role="tab" aria-controls="nav-xp" aria-selected="false" >Expériences</a>
+            <a class="nav-item nav-link" id="nav-languages-tab" data-toggle="tab" href="#nav-languages" role="tab" aria-controls="nav-languages" aria-selected="false" >Langues</a>
+            <a class="nav-item nav-link" id="nav-passions-tab" data-toggle="tab" href="#nav-passions" role="tab" aria-controls="nav-passions" aria-selected="false" >Passions</a>
           </div>
         </nav>
 
         <div class="tab-content" id="nav-tabContent">
-          <div class="tab-pane fade active show" id="nav-skills" role="tabpanel" aria-labelledby="nav-skills-tab" style="display:">
+          <div class="tab-pane fade active show" id="nav-skills" role="tabpanel" aria-labelledby="nav-skills-tab" >
             <p>
               <i class="fab fa-linux"></i>
               <span class="underline">OS :</span> Linux (Fedora, Debian), Windows, MacOS</p>
@@ -84,7 +73,7 @@
             <hr class="mt-5">
           </div>
 
-          <div class="tab-pane fade active show" id="nav-trainings" role="tabpanel" aria-labelledby="nav-trainings-tab" style="display:none">
+          <div class="tab-pane fade" id="nav-trainings" role="tabpanel" aria-labelledby="nav-trainings-tab">
             <p><span class="underline">nov 2020 - :</span>    19network42 coding school, Bruxelles</p>
             <p><span class="underline">oct 2020 - aout 2021:</span> Formation full-stack Valarep, Valenciennes</p>
             <p><span class="underline">avril - juin 2020 :</span> Formation découverte web dev, The Hacking Project</p>
@@ -94,7 +83,7 @@
             <hr class="mt-5">
           </div>
 
-          <div class="tab-pane fade active show" id="nav-xp" role="tabpanel" aria-labelledby="nav-xp-tab" style="display:none">
+          <div class="tab-pane fade" id="nav-xp" role="tabpanel" aria-labelledby="nav-xp-tab">
             <p><span class="underline">septembre 2020 :</span> piscine C à l'école 19, Bruxelles</p>
             <p><span class="underline">juillet 2020 :</span> Projet de site web pour l'association Cuisines Citoyennes Collaboratives</p>
             <p><span class="underline">janvier - mars 2020 :</span> Ouvrier serriste à Tom d'Aqui, Landes, France</p>
@@ -104,7 +93,7 @@
             <hr class="mt-5">
           </div>
 
-          <div class="tab-pane fade active show" id="nav-languages" role="tabpanel" aria-labelledby="nav-languages-tab" style="display:none">
+          <div class="tab-pane fade" id="nav-languages" role="tabpanel" aria-labelledby="nav-languages-tab">
             <p><span class="underline">Français</span> :
               <i class="fas fa-star"></i>
               <i class="fas fa-star"></i>
@@ -148,10 +137,9 @@
               <i class="far fa-star"></i>
             </p>
             <hr class="mt-5">
-
           </div>
 
-          <div class="tab-pane fade active show" id="nav-passions" role="tabpanel" aria-labelledby="nav-passions-tab" style="display:none">
+          <div class="tab-pane fade" id="nav-passions" role="tabpanel" aria-labelledby="nav-passions-tab">
             <p>
               <i class="fas fa-dice"></i>
               Jeux (vidéos, de cartes, de société, de rôles)
@@ -171,24 +159,20 @@
             <hr class="mt-5">
           </div>
         </div>
-
-
       </div>
     </div>
     <div class="text-center cv-footer">
       <div class="ikigai">
+        <h2 class="pt-5 underline">Mon Ikigai, ou ma raison d'être</h2>
         <img class="ikigai-picture" src="Screenshot_20201024_112006.png" alt="">
       </div>
-      <div class="pt-4 links" id="links">
-        <p>
-          <i class="fab fa-github"></i>
-           <a href="https://github.com/Aspinaut">https://github.com/Aspinaut</a></p>
-        <p>
-          <i class="fab fa-linkedin"></i>
-          <a href="https://www.linkedin.com/in/vincent-masse-701a931a3/">https://www.linkedin.com/in/vincent-masse-701a931a3/</a></p>
-        <p>
-          <span class="underline">Projet Final de formation :</span>
-          <a href="https://thp-final-project.herokuapp.com">https://thp-final-project.herokuapp.com</a></p>
+      <div class="pt-4 contact" id="contact">
+        <h2 class="mb-4 underline">Contact</h2>
+        <p>102, rue du Gras Boeuf</p>
+        <p>n°81, résidence les 5 pommes</p>
+        <p>59163 Condé sur l'Escaut, France</p>
+        <p>+33 6 28 53 65 98</p>
+        <p>vincentmasse@protonmail.com</p>
       </div>
     </div>
   </body>
@@ -200,10 +184,4 @@
   <!-- FONTAWESOME -->
   <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
 
-  <!-- NAVBAR -->
-  <script>
-  function manageNavbarContent(id) {
-    document.getElementById(id).style.display = "";
-  }
-  </script>
 </html>
